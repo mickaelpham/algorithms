36bfac66d2bf362782745a0da87fa40ae82ad7ac
Mickael Pham <mickael.pham@zuora.com>
2013-02-14 06:46:14 +0100

    Added UnionFind class

---
+++ src/UnionFind.java
@@ -0,0 +1,45 @@
+
+public class UnionFind {
+
+	/**
+	 * Initialize union-find data structure with N objects (0 to N-1)
+	 */
+	public UnionFind(int N) {
+		
+	}
+	
+	
+	/**
+	 * Add connection between p and q
+	 */
+	public void union(int p, int q) {
+		
+	}
+	
+	
+	/**
+	 * Are p and q in the same component?
+	 */
+	public boolean connected(int p, int q) {
+		
+		return false;
+	}
+	
+	
+	/**
+	 * Component identifier for p(0 to N-1)
+	 */
+	public int find(int p) {
+		
+		return 0;
+	}
+	
+	
+	/**
+	 * Number of components
+	 */
+	public int count() {
+		
+		return 0;
+	}
+}
